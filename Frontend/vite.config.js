e859973b7881ec93d58b016fe4cfb6600ec034aa
Shavan889 <shavansanhotra@gmail.com>
2026-05-27 13:06:27 +0530

    Fixed PWA build

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,23 +5,22 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    react(),tailwindcss(),
+    react(),
+    tailwindcss(),
 
     VitePWA({
       registerType: "autoUpdate",
 
-      includeAssets: [
-        "favicon.svg",
-        "robots.txt",
-        "favicon.png",
-      ],
+      includeAssets: ["favicon.png"],
 
       manifest: {
         name: "RYDO - Ride Your Dreams On",
         short_name: "RYDO",
         description: "My Awesome React PWA",
+
         theme_color: "#000000",
         background_color: "#ffffff",
+
         display: "standalone",
         scope: "/",
         start_url: "/",
